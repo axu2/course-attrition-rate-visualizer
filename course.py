f86a853e22b4acd5a421b6d9ed4d6432d823ddd6
ax2 <alexkurosakimh3@gmail.com>
2018-09-11 21:59:12 -0400

    convert to flask-sqlalchemy

--- course.py
+++ course.py
@@ -6,49 +6,47 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 import datetime
 import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-host = os.getenv('DATABASE_URL') or 'sqlite:///:memory:'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL') or 'sqlite:///:memory:'
+db = SQLAlchemy(app)
 
-engine = create_engine(host)
-Base = declarative_base()
-Session = sessionmaker(bind=engine)
-
-class Course(Base):
+class Course(db.Model):
     __tablename__ = 'courses'
 
-    id = Column(Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
 
-    code = Column(String)
-    title = Column(String)
+    code = db.Column(db.String)
+    title = db.Column(db.String)
 
-    max = Column(Integer)
+    max = db.Column(Integer)
 
     def __repr__(self):
         return "<Course(code={})>".format(self.code)
 
-class Enrollment(Base):
+class Enrollment(db.Model):
     __tablename__ = 'enrollments'
 
-    id = Column(Integer, primary_key=True)
-    date = Column(Date)
-    enroll = Column(Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    enroll = db.Column(db.Integer)
 
-    course_id = Column(Integer, ForeignKey('courses.id'))
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'))
 
-    course = relationship("Course", back_populates="enrollments")
+    course = db.relationship("Course", back_populates="enrollments")
 
     def __repr__(self):
         return "<Enrollment(enroll={})>".format(self.enroll)
 
-Course.enrollments = relationship("Enrollment", order_by=Enrollment.id, back_populates="course")
+Course.enrollments = db.relationship("Enrollment", order_by=Enrollment.id, back_populates="course")
 
-Base.metadata.create_all(engine)
+db.create_all()
 
 if __name__ == '__main__':
     import requests
     from bs4 import BeautifulSoup
-
-    session = Session()
 
     url = "https://registrar.princeton.edu/course-offerings/search_results.xml?submit=Search&term=1192&coursetitle=&instructor=&distr_area=&level=&cat_number=&subject=COS&sort=SYN_PS_PU_ROXEN_SOC_VW.SUBJECT%2C+SYN_PS_PU_ROXEN_SOC_VW.CATALOG_NBR%2CSYN_PS_PU_ROXEN_SOC_VW.CLASS_SECTION%2CSYN_PS_PU_ROXEN_SOC_VW.CLASS_MTG_NBR"
     html = requests.get(url).text
@@ -57,6 +55,6 @@ if __name__ == '__main__':
     for row in soup.table.findAll('tr')[1:]:
         cols = [list(col.stripped_strings) for col in row.findAll('td')]
         course = Course(id=cols[0][0], code=cols[1][0], title=cols[2][0], max=cols[9][0])
-        session.add(course)
+        db.session.add(course)
 
-    session.commit()
+    db.session.commit()
